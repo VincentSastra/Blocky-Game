ed0bc2456dc2768af6917a7e03f861a7962820c2
VincentSastra <vincent.sastra@gmail.com>
2020-11-03 00:36:43 -0800

    Added broken character movement & updated gitignore

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
+    public Rigidbody2D rb;
+    public float accel = 0.1f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb.velocity = new Vector2(0f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.UpArrow)|| Input.GetKeyDown(KeyCode.W))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + accel);
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow)|| Input.GetKeyDown(KeyCode.D))
+        {
+            rb.velocity = new Vector2(rb.velocity.x - accel, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+        {
+            rb.velocity = new Vector2(rb.velocity.x + accel, rb.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - accel);
+        }
         
+
     }
 }
