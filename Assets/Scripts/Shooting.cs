e23098d7cb30dd2e2301a873dde8bebeb76e375a
JASON-SASTRA-LA\jason sastra <jason121301@gmail.com>
2020-11-05 18:10:27 +0700

    Added shooting, shooting velocity isnt right

---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Transform bulletSpawnPoint;
+    public GameObject bulletPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+         //Shooting
+        if(Input.GetMouseButtonDown(0))
+        {
+
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2((Input.mousePosition.x - rb.position.x)/100f, (Input.mousePosition.y - rb.position.y)/100f);
+    }
+}
