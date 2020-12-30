d3137e274b9122280e5f8c7a37ee30c04bf3015a
VincentSastra <vincent.sastra@gmail.com>
2020-12-29 23:54:55 -0800

    Added camera movement script and picture

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] public GameObject centeredOn;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(centeredOn.transform.position.x, centeredOn.transform.position.y, transform.position.z);
+    }
+}
